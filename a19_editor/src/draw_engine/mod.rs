19944d8ff46fcaa8cd788a9e956bc53fb610bb71
Matt Hoyt <mrhoyt4@gmail.com>
2019-10-19 01:59:44 -0500

    Added missing classes.

--- a19_editor/src/draw_engine/mod.rs
+++ a19_editor/src/draw_engine/mod.rs
@@ -1,45 +1,54 @@
 pub mod event;
 pub mod shape;
 
+use crate::draw_engine::event:: {
+    KeyboardEvent
+};
+
+use crate::canvas:: {
+    DrawingCanvas
+};
+
 use a19_data_structure::graphic:: {
     Point
 };
 
-pub trait DrawingCanvas {
-    
-    /// Moves the current drawing location.
-    fn move_to(&mut self, point: &Point);
+/// Represents the engine for drawing on the canvas.
+pub struct DrawEngine<TCanvasContext: 'static>
+    where TCanvasContext: DrawingCanvas {
+    top_canvas: TCanvasContext,
+    primary_canvas: TCanvasContext,
+}
 
-    /// TUsed to draw a like to.
-    fn line_to(&mut self, point: &Point);
+pub fn create_draw_engine<TCanvasContext>(
+    top_canvas: TCanvasContext,
+    primary_canvas: TCanvasContext
+) -> DrawEngine<TCanvasContext> where TCanvasContext:DrawingCanvas {
+    DrawEngine {
+        top_canvas,
+        primary_canvas,
+    }
+}
 
-    /// Used to draw an arc.
-    fn arc(&mut self, point: &Point, radius: &f64, start_angle: &f64, end_angle: &f64);
+impl<TCanvasContext: DrawingCanvas> DrawEngine<TCanvasContext> {
 
-    /// Used to close a path.
-    fn close_path(&mut self);
+    pub fn mouse_move(&self, point: Point) {
+    }
 
-    /// Fills in a shape.
-    fn fill(&mut self);
+    pub fn mouse_down(&self, point: Point) {
 
-    /// Strokes a shape.
-    fn stroke(&mut self);
+    }
 
-    /// Used to fill in a style.
-    fn file_style(&mut self, value: &str);
+    pub fn mouse_up(&self, point: Point) {
 
-    /// Draws text on the screen at a position.
-    fn draw_text(&mut self, value: &str, point: &Point);
+    }
 
-    /// Used to mesaure the text.
-    fn measure_text(&mut self, value: &str) -> Option<f64>;
+    pub fn key_down<TEvent: KeyboardEvent>(&self, key_event: TEvent) {
 
-    /// Used to set the current font for the canvas
-    fn font(&mut self, font_name: &str);
+    }
 
-    /// Used to stroke text.
-    fn stroke_text(&mut self, value: &str, start: &Point);
+    pub fn key_up<TEvent: KeyboardEvent>(&self, key_event: TEvent) {
 
-    /// Sets the color to use when making a stroke, fill etc.
-    fn set_fill_color(&mut self, color: &str);
+    }
+
 }
