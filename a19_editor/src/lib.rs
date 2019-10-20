cd17596437abc7fa93fb5513ea95ac1112a8d9ef
Matt Hoyt <mrhoyt4@gmail.com>
2019-10-20 03:21:52 -0500

    Revert "Remove files so you can include the files in other workspaces."
    This reverts commit cabae20d8f752b82ad3bb349adda164a3f7f3489.

--- a19_editor/src/lib.rs
+++ a19_editor/src/lib.rs
@@ -1,6 +1,46 @@
 pub mod canvas;
 pub mod draw_engine;
 
+/// The different type of cursors.
+pub enum Cursor {
+    Auto,
+    Default,
+    None,
+    ContextMenu,
+    Help,
+    Pointer,
+    Progress,
+    Wait,
+    Cell,
+    Crosshair,
+    Text,
+    VerticalText,
+    Alia,
+    Copy,
+    Move,
+    NoDrop,
+    NotAllowed,
+    Grab,
+    Grabbing,
+    AllScroll,
+    ColResize,
+    RowResize,
+    NorthResize,
+    EastResize,
+    SouthResize,
+    WestResize,
+    NorthWestResize,
+    NorthEastResize,
+    SouthEastResize,
+    SouthWestResize,
+    EastWestResize,
+    NorthSouthResize,
+    NorthEastSouthWestResize,
+    NorthWestSouthEastReize,
+    ZoomIn,
+    ZoomOut
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
