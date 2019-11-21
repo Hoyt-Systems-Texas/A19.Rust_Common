71e25519f5bc74ac615534af936d1c6ccdd40d3d
Matt Hoyt <mrhoyt4@gmail.com>
2019-11-20 23:53:49 -0600

    Update the validation info.

--- a19_core/src/validation.rs
+++ a19_core/src/validation.rs
@@ -1,5 +1,23 @@
+/// Potential validation errors that we might have.
+#[derive(Debug)]
 pub enum ValidationError {
+    /// The value is required.
     Required,
-    MaxLength(u32),
-    MinLength(u32)
+    /// Expected an email address.
+    Email,
+    /// Exceeds the maximum length.
+    MaxLength(usize),
+    /// Doesn't meet the minimum length.
+    MinLength(usize),
+    /// Doesn't meet the minimum value.
+    MinValueU32(u32),
+    /// Doesn't meet the maximum value.
+    MaxValueU32(u32),
+    MinValueI32(i32),
+    MaxValueI32(i32),
+    MinValueUsize(usize),
+    MaxValueUsize(usize),
+    MaxValueU64(u64),
+    MinValueU64(u64)
 }
+
