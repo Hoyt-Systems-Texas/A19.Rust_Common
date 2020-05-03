07b8c937a934aa36822f1cd1831e0beec94a4fd1
Matt Hoyt <mrhoyt4@gmail.com>
2020-05-03 00:28:54 -0500

    Added the ability to use quotes for maching whole words.

--- a19_tokenizer/src/lib.rs
+++ a19_tokenizer/src/lib.rs
@@ -12,20 +12,42 @@ enum SearchTokenizerState {
     Hunting,
 }
 
+#[derive(PartialEq, Debug, Clone)]
+enum SearchGroupState {
+    None,
+    Grouping,
+}
+
 pub fn search_tokenizer<'a>(value: &'a str) -> Vec<SearchToken<'a>> {
     let mut start_index = 0;
     let mut tokens = Vec::with_capacity(10);
     let mut current_state = SearchTokenizerState::Hunting;
+    let mut group_state = SearchGroupState::None;
     for (p, c) in value.chars().enumerate() {
         match current_state {
             SearchTokenizerState::StartWith => {
                 if !c.is_alphanumeric() {
-                    tokens.push(SearchToken::StartsWith(&value[start_index..p-1]));
-                    current_state = SearchTokenizerState::Hunting;
+                    match group_state {
+                        SearchGroupState::Grouping => {
+                            if c == '"' {
+                                tokens.push(SearchToken::StartsWith(&value[start_index..p-1]));
+                                current_state = SearchTokenizerState::Hunting;
+                                group_state = SearchGroupState::None;
+                            } else {
+                                // Continue since not the end
+                            }
+                        }
+                        _ => {
+                            tokens.push(SearchToken::StartsWith(&value[start_index..p-1]));
+                            current_state = SearchTokenizerState::Hunting;
+                        }
+                    }
                 }
             }
             SearchTokenizerState::Hunting => {
-                if c.is_alphanumeric() {
+                if c == '"' {
+                    group_state = SearchGroupState::Grouping;
+                } else if c.is_alphanumeric() {
                     start_index = p;
                     current_state = SearchTokenizerState::Char;
                 }
@@ -34,8 +56,20 @@ pub fn search_tokenizer<'a>(value: &'a str) -> Vec<SearchToken<'a>> {
                 if c == '*' {
                     current_state = SearchTokenizerState::StartWith;
                 } else if !c.is_alphanumeric() {
-                    tokens.push(SearchToken::Value(&value[start_index..p]));
-                    current_state = SearchTokenizerState::Hunting;
+                    match group_state {
+                        SearchGroupState::Grouping => {
+                            if c == '"' {
+                                group_state = SearchGroupState::None;
+                                tokens.push(SearchToken::Value(&value[start_index..p]));
+                            } else {
+                                // Keep going
+                            }
+                        }
+                        SearchGroupState::None => {
+                            tokens.push(SearchToken::Value(&value[start_index..p]));
+                            current_state = SearchTokenizerState::Hunting;
+                        }
+                    }
                 }
             }
         }
@@ -85,5 +119,23 @@ mod tests {
     pub fn token_tests_no_numeric_starts_with_nums() {
         let tokens = search_tokenizer("Hello12* World*");
         assert_eq!(vec![SearchToken::StartsWith("Hello12"), SearchToken::StartsWith("World")], tokens);
+    }
+
+    #[test]
+    pub fn group_test_whole() {
+        let tokens = search_tokenizer("\"Hello World*");
+        assert_eq!(vec![SearchToken::StartsWith("Hello World")], tokens);
+    }
+    
+    #[test]
+    pub fn group_test_whole_mix() {
+        let tokens = search_tokenizer("\"Hello World*\" Something");
+        assert_eq!(vec![SearchToken::StartsWith("Hello World"), SearchToken::Value("Something")], tokens);
+    }
+
+    #[test]
+    pub fn group_test_whole_mix_starts() {
+        let tokens = search_tokenizer("\"Hello World*\" Something*");
+        assert_eq!(vec![SearchToken::StartsWith("Hello World"), SearchToken::StartsWith("Something")], tokens);
     }
 }
